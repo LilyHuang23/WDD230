65f36d90c41c222dce530b4e5714a748177f90f5
John Doe <l613426210@gmail.com>
2022-10-27 15:06:09 -0600

    lazyload

---
+++ lesson7/js/main.js
@@ -0,0 +1,31 @@
+let imgToLoad = document.querySelectorAll("img[data-src]");
+
+const loadImg = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () => {
+        image.removeAttribute("data-src");
+    };
+}
+
+imgToLoad.forEach((pictures) => {
+    loadImg(pictures);
+});
+
+if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((item, observer) => {
+        item.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImg(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    });
+    imgToLoad.forEach((img) => {
+        observer.observe(img);
+    });
+}
+else {
+    imgToLoad.forEach((img) => {
+        loadImg(img);
+    })
+}
