9856eabfddb498cfe7b916ecd49a104ff72c219c
John Doe <l613426210@gmail.com>
2022-10-30 21:20:06 -0600

    changes

--- chamber/js/discover.js
+++ chamber/js/discover.js
@@ -17,6 +17,14 @@ visit.textContent = visitMessage;
 window.localStorage.setItem("last-visit", theday.toString());
 
 // lazy load
+let imgToLoad = document.querySelectorAll("img[data-src]");
+
+const loadImg = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () => {
+        image.removeAttribute("data-src");
+    };
+}
 if ("IntersectionObserver" in window) {
     const observer = new IntersectionObserver((item, observer) => {
         item.forEach((item) => {
