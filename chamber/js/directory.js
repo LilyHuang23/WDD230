09d05ac3e6579e16506d4dd7d4df50b87cac6d2d
John Doe <l613426210@gmail.com>
2022-11-17 20:58:55 -0700

    weather api

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -4,16 +4,16 @@ let cardview = document.querySelector("#cardview");
 let listview = document.querySelector("#listview");
 
 cardselect.addEventListener('click', () => {
-    cardview.getElementsByClassName.display = 'grid';
-    listview.getElementsByClassName.display = 'none';
-    cardselect.getElementsByClassName.opacity = 1.0;
-    listselect.getElementsByClassName.opacity = 0.5;
+    cardview.style.display = 'grid';
+    listview.style.display = 'none';
+    cardselect.style.opacity = 1.0;
+    listselect.style.opacity = 0.5;
 });
 listselect.addEventListener('click', () => {
-    cardview.getElementsByClassName.display = 'none';
-    listview.getElementsByClassName.display = 'block';
-    cardselect.getElementsByClassName.opacity = 0.5;
-    listselect.getElementsByClassName.opacity = 1.0;
+    cardview.style.display = 'none';
+    listview.style.display = 'block';
+    cardselect.style.opacity = 0.5;
+    listselect.style.opacity = 1.0;
 });
 
 
@@ -52,27 +52,3 @@ const requestURL = "data/data.json";
         bizlist.forEach(displayCard);
         bizlist.forEach(displayList);
     });
-// main js
-
-const htoday = new Date();
-if (htoday.getDay()==4) {
-    document.querySelector(".banner").style.display="block";
-}
-// select the elements to manipulate (output to)
-const datefield = document.querySelector(".date");
-// derive the current date using a date object
-const now = new Date();
-const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
-datefield.innerHTML = `<em>${fulldate}</em>`;
-
-
-function toggleMenu(){
-    document.getElementById('primaryNav').classList.toggle('open');
-    document.getElementById('hamburgerBtn').classList.toggle('open');
-}
-const x = document.getElementById('hamburgerBtn');
-x.onclick = toggleMenu;
-// footer last update
-const today = new Date();
-document.querySelector("footer div span").textContent = today.getFullYear();
-document.getElementById("lastupdatedate").textContent = document.lastModified;
--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,10 +1,27 @@
+const lat = "64.8378";
+const lon = "-147.7164";
+const APIKEY= "5be535fa5e3977d7f2980c18ef561e11";
+const apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKEY}&units=imperial`;
+
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        // console.log(jsObject);
+        showWeather(jsObject);
+    });
 // windchill
-function setwindchill(temp, windspeed) {
+function showWeather(obj) {
     let tempobj = document.querySelector("#temperature");
     let windspeedobj = document.querySelector("#windspeed");
     let windchillobj = document.querySelector("#windchill");
+    let weathericon = document.querySelector("#weathericon"); 
+    let describe = document.querySelector("#describe"); 
+
+    const iconURL = `http://openweathermap.org/img/wn/${obj.weather[0].icon}@2x.png`;
 
     let windchillmsg = "N/A";
+    let temp = obj.main.temp;
+    let windspeed = obj.wind.speed;
 
     if (temp <= 50 && windspeed > 3) {
         let chill = Math.round((35.74 + (0.6215 * temp)) - (35.75 * Math.pow(windspeed, 0.16)) + (0.4275 * temp * Math.pow(windspeed, 0.16)));
@@ -14,6 +31,9 @@ function setwindchill(temp, windspeed) {
     tempobj.textContent = temp;
     windspeedobj.textContent = windspeed;
     windchillobj.innerHTML = windchillmsg;
+    weathericon.setAttribute("src", iconURL);
+    weathericon.setAttribute("alt", obj.weather[0].description);
+    describe = obj.weather[0].main;
 
 }
 // setwindchill(49, 10);
