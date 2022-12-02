83668bbe6d10400661547c9c89d5601c74f51059
John Doe <l613426210@gmail.com>
2022-12-01 17:36:01 -0700

    update

--- final/js/main .js
+++
@@ -1,5 +0,0 @@
-const today = new Date();
-document.querySelector("footer div span").textContent = today.getFullYear();
-document.getElementById("lastupdatedate").textContent = document.lastModified;
-// const lastModified = new Date(document.lastModified);
-// document.getElementById("lastupdatedate").textContent = `${lastmodified.getDate()}/${lastmodified.getMonth()+1}/${lastmodified.getFullYear()}` 
---
+++ final/js/main.js
@@ -0,0 +1,6 @@
+// hamburger Menu
+
+// footer
+const today = new Date();
+document.getElementById("footeryear").textContent = today.getFullYear();
+document.getElementById("lastupdatedate").textContent = document.lastModified;
