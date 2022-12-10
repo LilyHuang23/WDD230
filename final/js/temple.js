9e3429bf0d6a36d43832ad1a1b3bddc8a956686b
John Doe <l613426210@gmail.com>
2022-12-09 20:07:00 -0700

    finish temple page

--- final/js/temple.js
+++ final/js/temple.js
@@ -1,29 +1,31 @@
-const requestURL = 'https://www.churchofjesuschrist.org/temples/list';
-function displayTemples(temple) {  // Create elements to add to the document
-    let card = document.createElement('section');
-    let h2 = document.createElement('h2');    // Change the textContent property of the h2 element to contain the temple's full name
-    h2.textContent = `${temple.name}`;
-    // Add/append the section(card) with the h2 element
-    let p1 = document.createElement('p');
-    p1.textContent = `Address: ${temple.address}`;
-    let p2 = document.createElement('p');
-    p2.textContent = `Telephone: ${temple.telephone}`;
-    let p3 = document.createElement('p');
-    p3.textContent = `Services: ${temple.services}`;
-    let p4 = document.createElement('p');
-    p4.textContent = `Temple Closures: ${temple.templeclosures}`;
-    let img = document.createElement('img');
-    img.setAttribute('src', temple.imageURL);
-    
-    card.appendChild(h2);
-    card.appendChild(p1);
-    card.appendChild(p2);
-    card.appendChild(p3);
-    card.appendChild(p4);
-    card.appendChild(img);
-    // Add/append the existing HTML div with the cards class with the section(card)
-    document.querySelector('div.cards').appendChild(card);
-  }
+const requestURL = 'data/data.json';
+
+
+  // like 
+  // First check to see if we need to initialize local storage with an empty array
+const LIKES_KEY = "temple-likes";
+let likes_string = localStorage.getItem(LIKES_KEY);
+if (likes_string==null){
+    likes_string="[]";
+    localStorage.setItem(LIKES_KEY, likes_string);
+}
+
+// Turn the string value from local storage into a Java array
+let likeslist = JSON.parse(likes_string);
+
+// This displays the temple card as before
+function displayTemple(temples){
+    let main = document.querySelector("main");
+    let newsection = document.createElement("section");
+  newsection.innerHTML = `<img src="${temples.imageURL}">
+            <h2>${temples.names}</h2>
+            <p>Address: ${temples.address}</p>
+            <p>Telephone: ${temples.telephone}</p>
+            <p>Services: ${temples.services}</p>
+            <p>Temple Closures: ${temples.templeclosures}</p>
+            <input class="mycheck" id="check-${temples.names}" type="checkbox" onclick="likeTemple(this);"> Like This Temple!`;
+    main.appendChild(newsection);
+}
 fetch(requestURL)
   .then(function (response) {
     return response.json();
@@ -31,5 +33,35 @@ fetch(requestURL)
   .then(function (jsonObject) {
       console.table(jsonObject);  // temporary checking for valid response and data parsing
       let temples = jsonObject["temples"];
-      temples.forEach(displaytemples);
+      temples.forEach(displayTemple);
   });
+// This function handles when a user checks an individual checkbox
+// First, it updates the list of "liked" temples by either adding or removing it
+// depending on if the box is checked or unchecked.
+// push adds an item to a list
+// splice removes an item from a list.
+// Finally, the new list is put into local storage for later use. 
+function likeTemple(item){
+    let likes_string = localStorage.getItem(LIKES_KEY);
+    let likeslist = JSON.parse(likes_string);
+    if (item.checked){
+        if (!likeslist.includes(item.id)){
+            likeslist.push(item.id);
+        }
+    }
+    else{
+        if (likeslist.includes(item.id)){
+            likeslist.splice(likeslist.indexOf(item.id), 1);
+        }
+    }
+    localStorage.setItem(LIKES_KEY, JSON.stringify(likeslist));
+}
+
+// Upon page reload, the list of individual items (by id) is checked.
+function displayLike(item){
+    let obj = document.getElementById(item);
+    obj.checked = true;
+}
+
+temples.forEach(displayTemple);
+likeslist.forEach(displayLike);
